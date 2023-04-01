bcbf09b9ca145726b0d3d8f1f1ef1199d4b29e75
braj jha <chandnijha630@gmail.com>
2023-04-01 22:24:38 +0530

    uploading basics

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+let element=document.querySelectorAll('.gallery-items');
+let head=document.querySelector('.tag-line');
+head.addEventListener("scroll", function() {
+    if (head.scrollTop > 10) {
+   head.style.display="none";
+   head.style.visiblity="hidden";
+    }
+  });
+element.addEventListener("scroll", function() {
+    if (element.scrollTop > 10) {
+   element.style.display="none";
+   element.style.visiblity="hidden";
+    }
+  });*/
